b82e5c8399ef3852b974d4308a5ff9d6366b2279
Daniel Sagi <daniel.sagi@aquasec.com>
2022-04-03 22:03:35 +0300

    added config files for pypi metadata packages

--- hunting/dns_spoof_hunter/setup.py
+++ hunting/dns_spoof_hunter/setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 
 setup(
-    name="kube-hunter-dns-spoof",
     install_requires=["kube-hunter", "kube-hunter-arp-spoof", "scapy>=2.4.3"],
     entry_points={"kube_hunter": ["dns_entrypoint = dns_entrypoint"]},
     py_modules=["kube_hunter_dns_spoof"],
