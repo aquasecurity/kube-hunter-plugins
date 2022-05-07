4afe5d95cf8ffc81ad7cfe984a011f051ba05119
danielsagi <danielsagi2009@gmail.com>
2022-05-06 21:39:41 -0700

    changed names of main entrypoints to match package names

--- hunting/arp_spoof_hunter/setup.py
+++ hunting/arp_spoof_hunter/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     install_requires=["kube-hunter", "scapy>=2.4.3"],
-    entry_points={"kube_hunter": ["arp_entrypoint = arp_entrypoint"]},
+    entry_points={"kube_hunter": ["arp_spoof = kube_hunter_arp_spoof"]},
     py_modules=["kube_hunter_arp_spoof"],
 )
--- hunting/dns_spoof_hunter/setup.py
+++ hunting/dns_spoof_hunter/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     install_requires=["kube-hunter", "kube-hunter-arp-spoof", "scapy>=2.4.3"],
-    entry_points={"kube_hunter": ["dns_entrypoint = dns_entrypoint"]},
+    entry_points={"kube_hunter": ["dns_spoof = kube_hunter_dns_spoof"]},
     py_modules=["kube_hunter_dns_spoof"],
 )
